ee871d06e44da611813a04c32d818995252f00f0
TRezendes <78620355+TRezendes@users.noreply.github.com>
2022-07-27 17:58:31 -0400

    Just getting started

--- bot.py
+++ bot.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import json
+
+repeat = 10
+
+def RNoBot1(repeat):
+
+
+    https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=o14TzRzctK4QHyY7LQKTbPGzl2BquHcG
