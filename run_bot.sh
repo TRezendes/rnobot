1fc195934116775821c3aeb34e38df8429a1851c
TRezendes <78620355+TRezendes@users.noreply.github.com>
2022-07-29 04:01:26 +0000

    WOrking out the kinks of shell script

--- run_bot.sh
+++ run_bot.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/bash
 source /home/trezendes/.bashrc
-pyenv activate RNoBot
 cd /home/trezendes/rnobot
-/home/trezendes/.pyenv/shims/python /home/trezendes/rnobot/rnobot.py
+/home/trezendes/.pyenv/versions/RNoBot/bin/python /home/trezendes/rnobot/rnobot.py
 
